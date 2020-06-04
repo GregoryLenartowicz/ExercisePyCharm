9c2186f19cf0fc787fb577b3de6a8ef8936e2a85
Gregory <Lenartowicz>
2020-06-04 08:39:59 +0200

    dictionaries replay

--- Exercise_7 June 2020.py
+++ Exercise_7 June 2020.py
@@ -21,3 +21,16 @@ print(a)
 print(a.keys())
 print(a.values())
 
+#  from: https://docs.python.org/3.8/tutorial/datastructures.html?highlight=dictionary#dictionaries
+
+print('apple' in a)   # fast membership testing
+print('b' in a)
+
+val_expr = {i: i**2 for i in (2,4,8)}  # dict comprehensions (value)
+print(val_expr)
+
+key_expr = {i-4: i for i in (4,8,12)}  # dict comprehensions (key)
+print(key_expr)
+
+# 5.5. Dictionaries  https://docs.python.org/3.8/tutorial/datastructures.html?highlight=dictionary#dictionaries
+
---
+++ Exercise_8 June 2020.py
@@ -0,0 +1,14 @@
+# from: https://docs.python.org/3.8/tutorial/datastructures.html?highlight=dictionary#dictionaries
+
+# replay lists
+#  sets operations
+a = set('abcdacde')
+b = set('lkajd')
+print(a)  # random order of items
+c = a-b
+print(c)
+
+d = set('e')
+print(d)
+
+
