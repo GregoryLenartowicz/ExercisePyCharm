fb90b0103ed355f2c47db824f1c11453b400b047
Gregory <Lenartowicz>
2020-06-05 07:28:20 +0200

    no significant change

--- Exercise_7 June 2020.py
+++ Exercise_7 June 2020.py
@@ -32,5 +32,26 @@ print(val_expr)
 key_expr = {i-4: i for i in (4,8,12)}  # dict comprehensions (key)
 print(key_expr)
 
+r = dict([('ab',50), ("ad",100)]) # the dict constructor
+print(r)
+s = dict(ab=50, ad=100)   # easier
+print(s)
+
+
+# looping techniques
+dict_anim = {'cat':'big', 'mause':'small'}
+print(dict_anim)
+for k,v in dict_anim.items():   # items method
+    print(k,v)
+
+for i,v in enumerate(['a','b','c']):  # enumerate function
+    print(i,v)
+
+s = dict_anim['cat']
+print(s)
+
+# from: https://www.w3schools.com/python/python_dictionaries.asp
+
+
 # 5.5. Dictionaries  https://docs.python.org/3.8/tutorial/datastructures.html?highlight=dictionary#dictionaries
 
